3623e6f836ca909727b8e2c1416236abaf8057f9
fantasyinfo <gs27349gs@gmail.com>
2025-03-04 20:38:10 +0530

    vue js installed, and vite setup done for vue

---
+++ appointment-booking/resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    Hello from {{ _name }}
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            _name: 'Appointment booking'
+        }
+    }
+
+}
+</script>
